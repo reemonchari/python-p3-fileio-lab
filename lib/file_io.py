cadd9087aac96ed4c06f1880efef539d8093efac
+ <+>
2025-02-09 11:01:05 +0300

    File I/O

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    text_file = open(f"{file_name}.txt", mode = "w")
+    text_file.write(file_content)
+    text_file.close()
 
 def append_file(file_name, append_content):
-    pass
+    text_file = open(f"{file_name}.txt", mode = "a")
+    text_file.write(append_content)
+    text_file.close()
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", mode = "r") as text_file:
+        return text_file.read()
